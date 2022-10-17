7c27a9b1ab1225e36e407052d978c7a820663fef
Richard Chien <stdrc@outlook.com>
2022-10-17 15:27:05 +0800

    add doc comments
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/nodes/client/redis_like/mod.rs
+++ src/nodes/client/redis_like/mod.rs
@@ -1,3 +1,5 @@
+//! Provides Redis-like [`Client`], [`Connection`] and operations, etc.
+
 use anna_api::ClientKey;
 
 use crate::ClientConfig;
@@ -6,21 +8,25 @@ use self::convert::ToAnnaValue;
 
 mod convert;
 
+/// Redis-like client.
 pub struct Client {
     config: ClientConfig,
 }
 
 impl Client {
+    /// Creates a new client with given configuration.
     pub fn open(config: ClientConfig) -> eyre::Result<Self> {
         Ok(Self { config })
     }
 
+    /// Get an async connection object.
     pub async fn get_async_connection(&self) -> eyre::Result<Connection> {
         let client = crate::Client::new(self.config.clone())?;
         Ok(Connection { client })
     }
 }
 
+/// Async Redis-like connection to Anna cluster.
 pub struct Connection {
     client: crate::Client,
 }
